9a4ec9705c2c0cb1c81a359491d4dbb7b370a42d
Your Name <alexandermorgan364@gmail.com>
2025-01-18 20:52:32 -0500

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -14,6 +14,7 @@ import {
 import { usePathname } from "next/navigation";
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 // Type Defining our SidebarLink
 interface SidebarLinkProps {
@@ -77,7 +78,13 @@ const Sidebar = () => {
         className={`flex gap-3 justify-between md:justify-normal
          items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"} `}
       >
-        <div>logo</div>
+        <Image
+          src="https://s3-inventorymanagement4.s3.us-east-1.amazonaws.com/logo.png"
+          alt="morgstock-logo"
+          width={27}
+          height={27}
+          className="rounded w-8"
+        />
         <h1
           className={`${
             isSidebarCollapsed ? "hidden" : "block"
