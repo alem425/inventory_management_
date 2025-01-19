88dfa031d8f9999175ce6266c1a6f213a6e50d50
Your Name <alexandermorgan364@gmail.com>
2025-01-18 20:58:32 -0500

    added images

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  images: {
+    remotePatterns: [
+
+      {
+        protocol: "https",
+        hostname: "s3-inventorymanagement4.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
